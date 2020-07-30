216ed1b1da9fea0b8fbfbb56107480eb919e9f5e
Eric-ciccotti <eric.ciccotti@gmail.com>
2020-07-30 14:02:26 +0200

    add - modal improvement

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,5 +1,6 @@
 import { DataStorageService } from './../shared/data-storage.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-navigation',
@@ -7,18 +8,49 @@ import { Component, OnInit, ViewChild } from '@angular/core';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+  //variables necessaires pour le menu déroulant NG-bootstrap
   collapsed: boolean = true;
   isMenuCollapsed = true;
+  
+  //variable necessaires pour le MODAL NG-bootstrap
+  closeResult = '';
+  @ViewChild('content', { static: false, }) content: ElementRef;
 
+  //si return return FALSE = il y a 0 recette à sauvegarder ? donc on ouvre le modal
+  //sinon on sauvegarde
+  
   onSauvegarderRecette() {
-    this.dataSortageService.stockerRecette();
+    if (!this.dataSortageService.stockerRecette()) {
+      this.open(this.content);
+    } else {
+      this.dataSortageService.stockerRecette()
+    }
   }
 
   onRecupererRecette() {
     this.dataSortageService.recupererRecette().subscribe();
   }
 
-  constructor(private dataSortageService: DataStorageService) { }
+
+  open(content) {
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  constructor(private dataSortageService: DataStorageService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,7 +3,6 @@ import { RecettesService } from './recettes.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { tap } from "rxjs/operators";
-import { Ingredient } from './ingredient.model';
 import recetteBackup from './recette.backup';
 
 @Injectable({
@@ -14,17 +13,15 @@ export class DataStorageService {
   constructor(private http: HttpClient, private recetteService: RecettesService) { }
 
   stockerRecette() {
-
-
-
     let recette = this.recetteService.getRecette();
 
     if (recette.length === 0) {
       recette = recetteBackup;
+      return false;
+    } else {
+      this.http.put('https://livre-recette-exercice.firebaseio.com/recettes.json', recette).subscribe();
+      return true;
     }
-    this.http.put('https://livre-recette-exercice.firebaseio.com/recettes.json', recette).subscribe(response => {
-      console.log(response);
-    })
   }
 
   recupererRecette() {
